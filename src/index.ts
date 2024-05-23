e51df15adec1ab0e59bd729681bac004f8c3685b
breeiee <165183329+breeiee@users.noreply.github.com>
2024-05-23 13:38:45 +0900

    Initial setup (typescript, prettier) (#1)
    * chore: Add typescript, prettier
    
    * chore: Update prettier configuration
    
    * chore: Update typeScript configuration
    
    * chore: Update @types/jest
    
    * chore: Add path alias to typescript configuration
    
    * feat: Update title, Add header, Add style
    
    * chore: Change js to ts
    
    * chore: Update ts-loader
    
    * chore: Fix output

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import './index.css';
+
+const header = document.querySelector('header');
--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,45 @@
-import path from "path";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import { fileURLToPath } from "url";
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-  mode: "development",
-  entry: "./src/index.js",
+  mode: 'development',
+  entry: './src/index.ts',
   output: {
-    filename: "index.js",
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "/",
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
   },
   resolve: {
+    extensions: ['.ts', '.js'],
     alias: {
-      "@": path.resolve(__dirname, "src/"), // src 폴더를 '@'로 alias 설정
+      '@': path.resolve(__dirname, 'src/'), // src 폴더를 '@'로 alias 설정
     },
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.js$/,
+        test: /\.ts$/,
+        use: 'ts-loader',
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
       },
     ],
   },
-
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
   ],
   devServer: {
     historyApiFallback: true,
-    static: path.join(__dirname, "dist"),
+    static: path.join(__dirname, 'dist'),
     port: 9000,
   },
 };
